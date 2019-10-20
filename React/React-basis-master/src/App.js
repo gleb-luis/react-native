c8121dc5892aa5aec86ebb1d224f6f0f1591ffa0
glebluis <glebluis@hotmail.com>
2019-10-21 00:33:01 +0800

    commit

---
+++ React/React-basis-master/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './App.css';
+import ProductList from './js/app-1'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <ProductList />
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
